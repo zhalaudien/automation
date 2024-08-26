5864844d36d5895be620086691ffd375b375489a
zhalaudien <zhalaudien@gmail.com>
2024-08-26 08:40:10 +0700

    update php instalation

--- debian-server.sh
+++ debian-server.sh
@@ -4,8 +4,10 @@ echo -e "${c}Update dan Upgrade System"; $r
 sudo apt update && sudo apt upgrade && sudo apt autoremove -y
 
 echo -e "${c}install PHP"; $r
-sudo apt install software-properties-common apt-transport-https -y
-sudo add-apt-repository ppa:ondrej/php -y
+sudo apt install -y apt-transport-https lsb-release ca-certificates wget 
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list 
+sudo apt update
 sudo apt install apache2 php php-fpm php-gd php-xml php-mbstring php-curl php-zip php-intl -y
 
 echo -e "${c}restart Apache2"; $r
