782fbf95680e97a76b70535addd58afaeaff7759
zhalaudien <zhalaudien@gmail.com>
2024-08-26 08:27:11 +0700

    first commit

---
+++ debian-server.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -e "${c}Update dan Upgrade System"; $r
+sudo apt update && sudo apt upgrade && sudo apt autoremove -y
+
+echo -e "${c}install PHP"; $r
+sudo apt install software-properties-common apt-transport-https -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt install apache2 php php-fpm php-gd php-xml php-mbstring php-curl php-zip php-intl -y
+
+echo -e "${c}restart Apache2"; $r
+sudo systemctl restart apache2
+
+echo -e "${c}install composer"; $r
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+echo -e "${c}restart MariaDB-Server"; $r
+sudo apt install mariadb-server -y
+
+echo -e "${c}restart mariadb"; $r
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+echo -e "${c}setup mariadb"; $r
+mysql_secure_installation
+
+echo -e "${c}Install phpMyAdmin"; $r
+sudo apt install phpmyadmin -y
+
+echo -e "${c}restart Apache2"; $r
+sudo systemctl restart apache2
+
